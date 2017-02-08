dac8972843007dfc8e9ffd37445013b34646cac1
fin-alice <enderalice@yahoo.com>
2017-02-08 10:07:17 +0900

    Visual Studioでのデバッグ用にrundll32から実行可能に

---
+++ RunDll.cpp
@@ -0,0 +1,33 @@
+﻿#include "ClassDef.hpp"
+#include <winscard.h>
+#include <string>
+#include <vector>
+
+extern "C" void CALLBACK TestA(HWND hwnd, HINSTANCE hinst, LPSTR lpszCmdLine, int nCmdShow)
+{
+	MessageBoxW(hwnd, L"このメッセージはでないはずだよ。", nullptr, MB_ICONHAND);
+}
+
+extern "C" void CALLBACK TestW(HWND hwnd, HINSTANCE hinst, LPWSTR lpszCmdLine, int nCmdShow)
+{
+	if(IsDebuggerPresent())
+	{
+		__debugbreak();
+	}
+
+	SmartCardHelper * h = new SmartCardHelper();
+	LONG sc_result;
+
+	DebugPrint(L"%u of card readers installed on system.", h->GetReadersCount());
+
+	auto r = h->GetReaderAt(0);
+
+	sc_result = r->Connect();
+	if(sc_result != SCARD_S_SUCCESS)
+	{
+		MessageBoxFmt(hwnd, L"SuicaCP", MB_ICONASTERISK, L"SCardConnect-0x%x", sc_result);
+	}
+
+	delete h;
+
+}
