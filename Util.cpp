9828c2596072b3913c7108198efb38615b4affcb
fin-alice <enderalice@yahoo.com>
2017-02-28 12:19:36 +0900

    Util.cppを追加

---
+++ Util.cpp
@@ -0,0 +1,31 @@
+﻿#include <string>
+
+using std::wstring;
+
+void BinaryToString(const void * buf, size_t buf_size, wstring & str)
+{
+	static const wchar_t digit_t[] = {L'0', L'1', L'2', L'3', L'4', L'5', L'6', L'7', L'8', L'9', L'A', L'B', L'C', L'D', L'E', L'F'};
+	wchar_t digit[4];
+	digit[3] = L'\0';
+
+	str.clear();
+
+	for(size_t i = 0; i < buf_size; i++)
+	{
+		auto uc = static_cast<const unsigned char*>(buf)[i];
+		if(i == 0)
+		{
+			digit[0] = digit_t[(uc & 0xF0) >> 4];
+			digit[1] = digit_t[uc & 0x0F];
+			digit[2] = L'\0';
+		}
+		else
+		{
+			digit[0] = L' ';
+			digit[1] = digit_t[(uc & 0xF0) >> 4];
+			digit[2] = digit_t[uc & 0x0F];
+		}
+
+		str.append(digit);
+	}
+}
--- Util.hpp
+++ Util.hpp
@@ -1,10 +1,15 @@
 ﻿#pragma once
 #include <Windows.h>
+#include <string>
+
+using std::wstring;
 
 extern ULONG global_instances;
 
 extern "C" int MessageBoxFmt(HWND, const wchar_t *, UINT, const wchar_t *, ...);
 extern "C" void guid_to_string(const GUID &, wchar_t *);
+
+void BinaryToString(const void *, size_t, wstring &);
 
 #if defined(_DEBUG)
 extern "C" void DebugPrint(const wchar_t *, ...);
