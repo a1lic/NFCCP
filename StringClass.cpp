b958c692d87c2debe2e1a5e219091f502de989ae
a1lic <enderalice@yahoo.com>
2017-03-06 12:58:39 +0900

    文字列クラスを作成

---
+++ StringClass.cpp
@@ -0,0 +1,207 @@
+﻿#define STRINGCLASS_CPP
+#include "StringClass.hpp"
+#include "Util.hpp"
+
+using std::exception;
+
+ustring::ustring() : wstring()
+{
+	DebugPrint(L"ustring(0x%p)", this);
+	this->allocator = ustring::default_alloc;
+	this->releaser = ustring::default_free;
+}
+
+ustring::ustring(const wstring & right) : wstring(right)
+{
+	DebugPrint(L"ustring(0x%p, wstring(0x%p, \"%s\"))", this, &right, right.c_str());
+	this->allocator = ustring::default_alloc;
+	this->releaser = ustring::default_free;
+}
+
+ustring::ustring(const ustring & right) : wstring(right)
+{
+	DebugPrint(L"ustring(0x%p, ustring(0x%p, \"%s\"))", this, &right, right.c_str());
+	this->allocator = ustring::default_alloc;
+	this->releaser = ustring::default_free;
+}
+
+ustring::ustring(const wchar_t * s) : wstring(s)
+{
+	DebugPrint(L"ustring(0x%p, \"%s\")", this, s);
+	this->allocator = ustring::default_alloc;
+	this->releaser = ustring::default_free;
+}
+
+ustring::~ustring()
+{
+	DebugPrint(L"~ustring(0x%p, \"%s\")", this, this->c_str());
+}
+
+void * ustring::default_alloc(size_t s)
+{
+	return static_cast<void *>(new unsigned char[s]);
+}
+
+void ustring::default_free(void * b)
+{
+	delete[] reinterpret_cast<unsigned char *>(b);
+}
+
+UNICODE_STRING * ustring::to_unicode_string()
+{
+	if(IsBadCodePtr(reinterpret_cast<FARPROC>(this->allocator)))
+	{
+		// アロケーターが設定されていなければ既定のアロケーターを使用
+		this->allocator = ustring::default_alloc;
+	}
+	if(IsBadCodePtr(reinterpret_cast<FARPROC>(this->releaser)))
+	{
+		// リリーサーが設定されていなければ既定のリリーサーを使用
+		this->releaser = ustring::default_free;
+	}
+
+	auto str = static_cast<UNICODE_STRING*>((*this->allocator)(sizeof(UNICODE_STRING)));
+
+	if(!str)
+	{
+		// UNICODE_STRING構造体のバッファーを取れなかったら例外
+		throw exception(u8"[ustring] Can't allocate UNICODE_STRING structure.");
+	}
+
+	if(this->empty())
+	{
+		// 文字列無しだった場合、長さ0のUNICODE_STRINGを返す
+		str->Buffer = nullptr;
+		str->Length = 0;
+		str->MaximumLength = 0;
+
+		return str;
+	}
+
+	// UNICODE_STRINGを生成
+	{
+		// UNICODE_STRING構造体のLengthとMaximumLengthはバイト単位
+
+		// バイト単位で長さを取得し、32766文字以下になるようにする
+		auto wsize = sizeof(wchar_t) * this->size();
+		if(wsize > UNICODE_STRING_MAX_BYTES)
+		{
+			wsize = UNICODE_STRING_MAX_BYTES;
+		}
+
+		// 32766文字以下に切り詰め
+		str->Length = static_cast<USHORT>(wsize);
+		str->MaximumLength = static_cast<USHORT>(wsize);
+
+		str->Buffer = static_cast<wchar_t*>((*this->allocator)(wsize));
+
+		if(!str->Buffer)
+		{
+			// 実際の文字列を保持するバッファーを取れなかった場合は例外
+			(*this->releaser)(str);
+			throw exception(u8"[ustring] Can't allocate string buffer.");
+		}
+	}
+
+	// UNICODE_STRINGの文字列にコピー
+	memcpy(str->Buffer, this->c_str(), str->Length);
+
+	return str;
+}
+
+LSA_STRING * ustring::to_lsa_string()
+{
+	if(IsBadCodePtr(reinterpret_cast<FARPROC>(this->allocator)))
+	{
+		// アロケーターが設定されていなければ既定のアロケーターを使用
+		this->allocator = ustring::default_alloc;
+	}
+	if(IsBadCodePtr(reinterpret_cast<FARPROC>(this->releaser)))
+	{
+		// リリーサーが設定されていなければ例外
+		this->releaser = ustring::default_free;
+	}
+
+	auto str = static_cast<LSA_STRING*>((*this->allocator)(sizeof(LSA_STRING)));
+
+	if(!str)
+	{
+		// UNICODE_STRING構造体のバッファーを取れなかったら例外
+		throw exception(u8"[ustring] Can't allocate LSA_STRING structure.");
+	}
+
+	if(this->empty())
+	{
+		str->Buffer = nullptr;
+		str->Length = 0;
+		str->MaximumLength = 0;
+
+		return str;
+	}
+
+	auto wsize = static_cast<int>(this->size());
+
+	// UTF-7に変換した際のサイズをチェック
+	auto newsize = WideCharToMultiByte(CP_UTF7, 0, this->c_str(), wsize, nullptr, 0, nullptr, nullptr);
+	if(newsize <= 0)
+	{
+		// 変換結果が空文字列の場合は終了
+		str->Buffer = nullptr;
+		str->Length = 0;
+		str->MaximumLength = 0;
+
+		return str;
+	}
+
+	if(newsize > USHRT_MAX)
+	{
+		// LSA_STRINGは65535文字までなので、超えていた場合は切り捨てる
+		newsize = USHRT_MAX;
+	}
+
+	// LSA_STRINGを生成
+	str->Length = newsize;
+	str->MaximumLength = newsize;
+	str->Buffer = static_cast<char*>((*this->allocator)(newsize));
+
+	if(!str->Buffer)
+	{
+		// 文字列を格納するバッファーを取れなかった場合はLSA_STRINGを破棄
+		(*this->releaser)(str);
+		throw exception(u8"[ustring] Can't allocate string buffer.");
+	}
+
+	// 変換実行
+	WideCharToMultiByte(CP_UTF7, 0, this->c_str(), wsize, str->Buffer, newsize, nullptr, nullptr);
+
+	return str;
+}
+
+ustrings::ustrings()
+{
+	DebugPrint(L"ustrings(0x%p)", this);
+}
+
+ustrings::~ustrings()
+{
+	//this->clear();
+	DebugPrint(L"~ustrings(0x%p, %Iu)", this, this->size());
+}
+
+void ustrings::push_back(const wchar_t * s)
+{
+	auto ss = new ustring(s);
+	vector::push_back(*ss);
+}
+
+/*void ustrings::clear()
+{
+	DebugPrint(L"ustrings(0x%p)::clear() size=%Iu", this, this->size());
+
+	while(!this->empty())
+	{
+		auto i = this->back();
+		delete i;
+		this->pop_back();
+	}
+}*/
---
+++ StringClass.hpp
@@ -0,0 +1,63 @@
+﻿#pragma once
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <Windows.h>
+#include <NTSecAPI.h>
+
+using std::default_delete;
+using std::wstring;
+using std::vector;
+using std::shared_ptr;
+
+typedef void * (* ustring_alloc)(size_t);
+typedef void   (* ustring_free )(void *);
+
+class ustring : public wstring
+{
+private:
+	ustring_alloc allocator;
+	ustring_free  releaser;
+public:
+	ustring();
+	ustring(const wstring &);
+	ustring(const ustring &);
+	ustring(const wchar_t *);
+	virtual ~ustring();
+
+	inline void set_allocater(ustring_alloc a, ustring_free f) { this->allocator = a; this->releaser = f; }
+
+	inline operator size_t() { return this->size(); }
+	inline operator const wchar_t *() { return this->c_str(); }
+
+	UNICODE_STRING * to_unicode_string();
+	inline operator UNICODE_STRING *() { return this->to_unicode_string(); }
+
+	LSA_STRING * to_lsa_string();
+	inline operator LSA_STRING *() { return this->to_lsa_string(); }
+
+	inline operator shared_ptr<ustring>() { return shared_ptr<ustring>(this); }
+
+	static void * default_alloc(size_t);
+	static void default_free(void *);
+};
+
+class ustrings : public vector<shared_ptr<ustring>>
+{
+public:
+	ustrings();
+	ustrings(const ustrings &) = default;
+	ustrings & operator =(const ustrings &) = default;
+	virtual ~ustrings();
+
+	void push_back(const wchar_t *);
+};
+
+#if defined(STRINGCLASS_CPP)
+template class shared_ptr<ustring>;
+template class vector<shared_ptr<ustring>>;
+#else
+extern template class shared_ptr<ustring>;
+extern template class vector<shared_ptr<ustring>>;
+#endif
